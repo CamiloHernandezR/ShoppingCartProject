89deb2418d27d114283a4a80e6af26123b076e1e
Camilo Hernandez <camilohenbor@gmail.com>
2022-05-23 22:27:31 -0500

    :sparkles: adding GetProducts API method

--- ShopOnlineSolution/ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnlineSolution/ShopOnline.Api/Controllers/ProductController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Extensions;
+using ShopOnline.Api.Repositories.Contracts;
+using ShopOnline.Models.Dtos;
 
 namespace ShopOnline.Api.Controllers
 {
@@ -7,5 +11,34 @@ namespace ShopOnline.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetItems()
+        {
+            try
+            {
+                IEnumerable<Product> products =await  this.productRepository.GetProducts();
+                IEnumerable<ProductCategory> categories = await this.productRepository.GetCategories();
+                if(products==null || categories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    IEnumerable<ProductDto> productDtos = products.GetProductDtos(categories);
+                    return Ok(productDtos);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500,ex.Message);
+            }
+        }
     }
 }
