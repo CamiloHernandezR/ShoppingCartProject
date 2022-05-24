1e17dc6e26efe3fca3fcc4df0a712b1e97f1b97b
Camilo Hernandez <camilohenbor@gmail.com>
2022-05-23 21:07:51 -0500

    :card_file_box: Creating DTOs items

--- ShopOnlineSolution/ShopOnline.Api/Entities/CartItem.cs
+++ ShopOnlineSolution/ShopOnline.Api/Entities/CartItem.cs
@@ -2,5 +2,10 @@
 {
     public class CartItem
     {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int productId { get; set; }
+        public int Qty { get; set; }
+
     }
 }
